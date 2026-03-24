bcf5a0f6b4bee894d49ea9610cf98b3c396bec53
Mirko <py1colommi@group.pirelli.com>
2026-03-24 12:12:24 +0100

    fix: fixed tests

--- test/mocks/mocks.dart
+++ test/mocks/mocks.dart
@@ -1,4 +1,5 @@
 import 'package:mocktail/mocktail.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:task_master_app/spes_app/models/product.dart';
 import 'package:task_master_app/spes_app/models/store.dart';
 import 'package:task_master_app/spes_app/models/price_history.dart';
@@ -9,6 +10,10 @@ class FakeStore extends Fake implements Store {}
 class FakePriceHistory extends Fake implements PriceHistory {}
 
 void registerTestMocks() {
+  // Inizializza sqflite per l'uso con FFI (necessario nei test su Windows/Linux)
+  sqfliteFfiInit();
+  databaseFactory = databaseFactoryFfi;
+
   registerFallbackValue(FakeProduct());
   registerFallbackValue(FakeStore());
   registerFallbackValue(FakePriceHistory());
--- test/widgets/current_shopping_screen_test.dart
+++ test/widgets/current_shopping_screen_test.dart
@@ -8,6 +8,8 @@ import 'package:task_master_app/spes_app/providers/product_provider.dart';
 import 'package:task_master_app/spes_app/models/store.dart';
 import 'package:task_master_app/spes_app/models/product.dart';
 import 'package:task_master_app/spes_app/constants/app_strings.dart';
+import '../mocks/mocks.dart';
+
 
 // Notifiers per i test
 class TestCartNotifier extends CartNotifier {
@@ -35,20 +37,27 @@ class TestActiveStoreNotifier extends ActiveStoreNotifier {
 }
 
 void main() {
-  testWidgets('CurrentShoppingScreen should show empty message when cart is empty', (tester) async {
-    await tester.pumpWidget(
-      ProviderScope(
-        overrides: [
-          activeStoreIdProvider.overrideWith(() => TestActiveStoreNotifier('1')),
-          storeProvider.overrideWith(TestStoreNotifier.new),
-          productProvider.overrideWith(TestProductNotifier.new),
-          cartProvider.overrideWith(() => TestCartNotifier([])),
-        ],
-        child: const MaterialApp(
-          home: CurrentShoppingScreen(),
-        ),
+  setUpAll(() {
+    registerTestMocks();
+  });
+
+  Widget createTestableWidget({List<CartItem> items = const []}) {
+    return ProviderScope(
+      overrides: [
+        activeStoreIdProvider.overrideWith(() => TestActiveStoreNotifier('1')),
+        storeProvider.overrideWith(TestStoreNotifier.new),
+        productProvider.overrideWith(TestProductNotifier.new),
+        cartProvider.overrideWith(() => TestCartNotifier(items)),
+      ],
+      child: const MaterialApp(
+        home: Scaffold(body: CurrentShoppingScreen()),
       ),
     );
+  }
+
+  testWidgets('CurrentShoppingScreen should show empty message when cart is empty', (tester) async {
+    await tester.pumpWidget(createTestableWidget(items: []));
+
 
     await tester.pump();
     await tester.pump(const Duration(milliseconds: 500));
@@ -60,19 +69,8 @@ void main() {
       CartItem(id: '1', barcode: '123', name: 'Pasta', price: 1.50)
     ];
 
-    await tester.pumpWidget(
-      ProviderScope(
-        overrides: [
-          activeStoreIdProvider.overrideWith(() => TestActiveStoreNotifier('1')),
-          storeProvider.overrideWith(TestStoreNotifier.new),
-          productProvider.overrideWith(TestProductNotifier.new),
-          cartProvider.overrideWith(() => TestCartNotifier(items)),
-        ],
-        child: const MaterialApp(
-          home: CurrentShoppingScreen(),
-        ),
-      ),
-    );
+    await tester.pumpWidget(createTestableWidget(items: items));
+
 
     await tester.pump();
     await tester.pump(const Duration(milliseconds: 500));
